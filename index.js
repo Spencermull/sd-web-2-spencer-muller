8920737512ae50d36c3305916d63c40d4a6cc9a2
Spencer Muller <spencer.muller@edu.sait.ca>
2025-09-10 20:36:17 -0600

    created the function requested in exercise 3 by reusing the logic from previous exercise in order to create a list element and just basically added an array parameter in the function so that you can pass any array through

--- index.js
+++ index.js
@@ -35,7 +35,16 @@ const users = [
     });
 
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
+    function renderArray(array){
+      for(let i = 0; i < array.length; i++){
+      const listElement = document.createElement("li");
+      listElement.textContent = array[i].name
+      document.getElementById("function-list").append(listElement);
+      }
+    }
 
+    renderArray(users); //Test array case
+    
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
 
 // 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
